7d32495e26a648ada742a7355e359ee7fc236848
FARSI Assim <hamriassim@gmail.com>
2026-01-11 18:07:05 +0100

    Added conditionnal branches : First properly Turing complete version !

--- cpu.py
+++ cpu.py
@@ -9,10 +9,11 @@ from arith_unit import adder
 from regs import registers
 from mux import mux
 from alu import alu
+from log_unit import b_or
 
 def decoder(instruction):
     x = instruction
-    sizes = [5, 5, 5, 10, 3, 1, 1, 1, 1]
+    sizes = [5, 5, 5, 8, 3, 1, 1, 1, 3]
     positions = [32 for _ in range(len(sizes) + 1)]
     for i in range(len(sizes)):
         positions[i+1] = positions[i] - sizes[i]
@@ -29,23 +30,27 @@ def cpu():
     ram_addr_size = 10
     # reg_addr_size = 5
 
-    branch_en = Defer(1, lambda: is_branch)
+    branch_en = Defer(1, lambda: condition_met )
     data_in = Defer(n, lambda: new_rd) 
     (vs1, vs2, pc) = registers(Defer(5, lambda:rd), data_in, [Defer(5, lambda:rs1), Defer(5, lambda:rs2)], branch_en)
     instruction = ROM(rom_addr_size, instruction_size, pc[:rom_addr_size])
-    rs1,rs2,rd,imm,op,is_imm, write_to_ram, read_from_ram, is_branch = decoder(instruction)
+    rs1,rs2,rd,imm,op,is_imm, write_to_ram, read_from_ram, condition = decoder(instruction)
+    
     completed_imm = imm + Constant((n-imm.bus_size)*"0")
+    is_branch = b_or(condition)
     va = mux(is_imm, vs1+completed_imm)
     vb = vs2
     result, eq, lt, ltu = alu(va, vb, op)
+    condition_met = condition[0] ^ (mux(condition[1:], Constant("0")+eq+lt+ltu))
     data = RAM(ram_addr_size, data_size, result[:ram_addr_size], write_to_ram, result[:ram_addr_size], vs1)
-    new_rd = mux(read_from_ram, result+data)
-    return va, vb
+    new_rd = mux(read_from_ram, mux(is_branch, result+completed_imm)+data)
+    return va, vb, pc, condition_met
 
 def main() :
-    v1,v2 = cpu()
+    v1,v2, pc, is_jmp = cpu()
     v1.set_as_output("v1")
     v2.set_as_output("v2")
-    # pc.set_as_output("pc")
+    pc.set_as_output("pc")
+    is_jmp.set_as_output("jmp")
     # ra.set_as_output("ra")
 
