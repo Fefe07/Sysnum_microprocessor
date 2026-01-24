878e5446c95d6ec60a585c1cc4b5ec12ef461dd6
FARSI Assim <hamriassim@gmail.com>
2026-01-24 16:58:15 +0100

    added signed division

--- divider.py
+++ divider.py
@@ -25,14 +25,35 @@ def unsigned_divider_aux(a, b):
     return div_l+subtract_b, mod_l
 
 def unsigned_divider(a, b):
+    assert a.bus_size == b.bus_size
     n = a.bus_size
     return unsigned_divider_aux(a, Constant((n-1)*"0") + b)
+
+def conditionnal_neg(x, negate):
+    n = x.bus_size
+    return arith_unit(Constant(n*"0"), x, negate)[0]
+
+def signed_divider(a, b):
+    assert a.bus_size == b.bus_size
+    n = a.bus_size
+    a_abs = conditionnal_neg(a, a[n-1])
+    b_abs = conditionnal_neg(b, b[n-1])
+    div, rem = unsigned_divider(a_abs, b_abs)
+    return conditionnal_neg(div, a[n-1] ^ b[n-1]), conditionnal_neg(rem, a[n-1])
+
+def divider(a, b, is_signed): 
+    n = a.bus_size
+    a_abs = conditionnal_neg(a, a[n-1] & is_signed)
+    b_abs = conditionnal_neg(b, b[n-1] & is_signed)
+    div, rem = unsigned_divider(a_abs, b_abs)
+    return conditionnal_neg(div, (a[n-1] ^ b[n-1]) & is_signed ), conditionnal_neg(rem, a[n-1] & is_signed)
 
 def main() :
     n = 8
     a = Input(n)
     b = Input(n)
-    div, rem = unsigned_divider(a, b)
-    div.set_as_output("div")
+    is_signed = Input(1)
+    div, rem = divider(a, b, is_signed)
+    div.set_as_output("quotient")
     rem.set_as_output("remainder")
 
