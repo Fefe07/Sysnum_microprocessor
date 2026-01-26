a81e6ccc4fd74be3476323d34e5912a80f59b750
Fefe07 <felixlandreau97@gmail.com>
2026-01-26 12:20:17 +0100

    cleaned the compiler.py file, fixed the laebl syntax

--- compiler.py
+++ compiler.py
@@ -10,6 +10,7 @@ from instructions import *
 from log_unit import clone
 
 def print_prog_file(p, f):
+    # Imprime le programme construit p dans le fichier de sortie f
     need_pos = False
     for n,i in enumerate(p):
         if i != '0':
@@ -21,28 +22,29 @@ def print_prog_file(p, f):
     f.write("\n")
 
 def get_line_words(file_in) :
+    # Renvoie la prochaine ligne non blanche et la liste de ses mots, sans virgule
 
     words = []
 
-
+    # élimination des lignes blanches
     while words == [] : 
         line = file_in.readline()
         while line == "" :
             line = file_in.readline()
             if not line :
                 return None, None
+
         if line[-1]=='\n' :
             line = line[:len(line)-1]
         line = line.replace(',', ' ')
-        
-
-        print(line)
+        #print(line)
         words = line.split(' ')
         while '' in words :
             words.remove('')
+    
 
     for i in range(1,len(words)) :
-#gestion des alias 
+    #gestion des alias 
         words[i] = words[i].replace('zero', 'x0')
         words[i] = words[i].replace('ra', 'x1')
         words[i] = words[i].replace('sp', 'x2')
@@ -77,12 +79,12 @@ def get_line_words(file_in) :
         words[i] = words[i].replace('t5', 'x30')
         words[i] = words[i].replace('t6', 'x31')
 
-    
     #print(line)
     return line, words
     
 def get_val_from_label(label, labels, labels_num, n) :
-    #label = words[1][1:-1]
+    # Permet de calculer le décalage relatif à partir du label
+    # Gros calculs à cause de la fonctionnalité 1b/1f
 
     if int(label[0]) in list(range(1,10)):
         if len(label)!=2 :
@@ -121,80 +123,55 @@ def get_val_from_label(label, labels, labels_num, n) :
 
 def compile(filename) :
 
-    # Premier passage pour repérer les labels
+    ## Premier passage pour repérer les labels
+
     labels = {}
     labels_num = 9*[[]]
 
     program = []
     file_in = open(filename, "r")
-    # line = file_in.readline()
-    # while line and line == "\n" :
-    #     line = file_in.readline()
     line,words = get_line_words(file_in)
+
+    # n compte les lignes de code produit
     n = 0
     while line :
         #words = get_line_words(file_in)
-        if words[0][0] == '.' :
-            name = words[0][1:]
-            if name[-1]==':' :
-                name = name[:len(name)-1]
+        if words[0][-1] == ':' :
+            name = words[0][:-1]
+            # if name[-1]==':' :
+            #     name = name[:len(name)-1]
             if int(name) in list(range(1,10)) :
                 labels_num[int(name)-1].append(n)
             else :
                 labels[name]= n 
-            # program.append()
         else :
-            # n compte les lignes de code
+            # n compte uniquement les lignes de code produit
             n += 1
-        # line = file_in.readline()
-        # while line and line == "\n" :
-        #     line = file_in.readline()
+
         line,words = get_line_words(file_in)
     file_in.close()
-    print("labels :")
-    print(labels)
-    print("labels_num :")
-    print(labels_num)
+    # print("labels :")
+    # print(labels)
+    # print("labels_num :")
+    # print(labels_num)
 
+
+    ## Second passge : construction du programme 
 
     program = []
     file_in = open(filename, "r")
     file_out = open("compile.out", "w")
+    # n compte les lignes de code produit
     n = 0
-    # line = file_in.readline()
-    # while line and line == "\n" :
-    #     line = file_in.readline()
+
     line,words = get_line_words(file_in)
 
-    while line!=None :
-        #print(line)
-        #words = get_words(line)
-        #print(words[0])
+    while line != None :
 
-        # if words[0][0] == "." :
-        #     # On ignore la ligne car les labels ont déjà été traités
-        #     ()
-
-        # elif words[1][0] == '"' :
-        #     # labels après un jmp
-        #     label = words[1][1:len(words[1])-1]
-
-        #     if words[0] == "jmp" :
-        #         print("On a trouvé un jmp")
-        #         program.append(jump(0,4*(labels[label]-n)))
-        #         n +=1
-
-        # else :
-        # if words[1][0] == '$' :
-        #     #constantes
-        #     is_cst_1 = True
-        #     val_1 = int(words[1][1:])
-        #     #print(val_1)
         if len(words)>=2 :
             if words[1][0] == "x" :
                 #registres
-                #is_cst_1 = False 
-                #print("mords[1][2:] = ", words[1][2:])
+                #print("words[1][2:] = ", words[1][2:])
                 val_1 = int(words[1][1:])
                 is_cst_1 = False
             
@@ -206,7 +183,7 @@ def compile(filename) :
             else :
                 val_1 = int(words[1])
                 is_cst_1 = True
-            print("val_1 = ", val_1)
+            #print("val_1 = ", val_1)
 
             if len(words) >= 3 :
 
@@ -218,10 +195,9 @@ def compile(filename) :
                     val_2 =  get_val_from_label(words[2][1:-1], labels, labels_num, n)
                     is_cst_2 = True
                 else :
-                    #assert(False)
                     val_2 = int(words[2])
                     is_cst_2 = True
-                print("val_2 = ", val_2)
+                # print("val_2 = ", val_2)
 
                 if len(words) == 4 :
                     assert(not is_cst_2)
@@ -235,19 +211,13 @@ def compile(filename) :
                     else :
                         val_3 = int(words[3])
                         is_cst_3 = True
-                    print("val_3 = ", val_3)
+                    #print("val_3 = ", val_3)
 
-                    # if is_cst_3 : 
-                    print(is_cst_3)
+                    #print(is_cst_3)
                     match (words[0],is_cst_3) :
-                        # attention : op_imm(op,dest,src,imm) (inversés par rapport à reg)
+                        
                         case ("addi",True) :
-                            
                             program.append(op_imm("add",val_1 ,val_2 ,val_3 ))
-                        # case "sub",True :
-                        #     program.append(op_imm("sub",val_1 ,val_2 ,val_3 ))
-                # else :
-                #     match words[0] :
                         case ("add", False) :
                             program.append(op_reg("add",val_1 ,val_2 ,val_3 ))
                         case ("sub", False) :
@@ -322,11 +292,6 @@ def compile(filename) :
                 else :
                     assert(len(words) == 3)
                     match words[0], is_cst_2 :
-                        # case "mov", True :
-                        #     #mov demande la destination en premier
-                        #     program.append(mov_imm(val_2,val_1))
-                        # case "mov", False :
-                        #         program.append(mov_reg(val_2,val_1))
                         case ("jal", True) :
                             program.append(jump(val_1, val_2))
                         case("lui", True) :
@@ -337,6 +302,8 @@ def compile(filename) :
                             program.append(mov_imm(val_1, val_2))
                         case("mov", False) :
                             program.append(mov_reg(val_1, val_2))
+                        case("jr", True) :
+                            program.append(jump_reg(0, val_1, val_2))
                         case _ : 
                             print("Opération non traitée")
                             assert(False)
@@ -349,6 +316,8 @@ def compile(filename) :
                         program.append(pop(val_1))
                     case("push", False) :
                         program.append(push(val_1))
+                    case("j", True) :
+                        program.append(jump(0,val_1))
 
 
             else :
@@ -358,25 +327,22 @@ def compile(filename) :
                 case("ret") :
                     program.append(ret())
             
-            if words[0][0] == "." :
-                n -= 1 
-        n+=1
+            
+        if words[0][-1] != ":" :
+            # n compte uniquement les lignes de code produit
+            n+=1
         
 
-        # line = file_in.readline()
-        # while line and line == "\n" :
-        #     line = file_in.readline()
         line, words = get_line_words(file_in)
 
-    print_prog(program)
+    #print_prog(program)
     print_prog_file(program,file_out)
-    #print_prog_file(prog_test_jmp, file_out)
 
-    print("Fin du fichier")
+    #print("Fin du fichier")
 
     file_in.close()
 
-
+# Modifier cette ligne pour compiler un autre code
 compile("compiler_test.ass")
 
 
