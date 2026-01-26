bd6f044bd1f7c723f3d480d59e1b86391dd00905
FARSI Assim <hamriassim@gmail.com>
2026-01-26 21:48:04 +0100

    put the netlist cpu in a a directory

---
+++ netlist_cpu/config.mk
@@ -0,0 +1,4 @@
+CAROTTE_PATH = ../carotte/carotte.py # Mettez le chemin relatif vers votre carotte.py 
+PYTHON_INTERPRETER = python3 # Si ça marche pas chais vous, essayez avec "python" ou "py"
+SIMULATOR = ../simulator/netlist_simulator.byte # Mettez le chemin vers votre simulateur netlist
+SIMULATOR_DIR = ../simulator
