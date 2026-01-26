1861f3c5d65855b7fe47bec833e00ea786565792
Dihellgo <92475915+Dihellgo@users.noreply.github.com>
2026-01-26 17:43:48 +0100

    used labels in clocks

--- clock.s
+++ clock.s
@@ -4,7 +4,7 @@
   addi x4, x0, 60
   addi x5, x0, 1
   addi x6, x0, 24
-boucle:
+1:
   sw x3, x0, 4
   sw x5, x0, 0
   sw x2, x0, 4
@@ -12,12 +12,12 @@ boucle:
   sw x1, x0, 4
   sw x5, x0, 0
   addi x1, x1, 1
-  bne x1, x4, -28
+  bne x1, x4, "1b"
   addi x1, x0, 0
   addi x2, x2, 1
-  bne x2, x4, -40
+  bne x2, x4, "1b"
   addi x2, x0, 0
   addi x3, x3, 1
   bne x3, x6, -52
   addi x3, x0, 0
-  jal x7, -60
+  jal x7, "1b"
--- clock_reel_time.s
+++ clock_reel_time.s
@@ -5,8 +5,9 @@
   addi x9, x0, 24
   mul x10, x8, x9
   lw x2, x0, 8
+1:
   lw x3, x0, 8
-  beq x3, x2, -4
+  beq x3, x2, "1f"
   rem x4, x3, x10
   addi x2, x3, 0
   div x5, x4, x8
@@ -19,4 +20,4 @@
   rem x5, x4, x7
   sw x5, x0, 4
   sw x6, x0, 0
-  jal x12, -56
+  jal x12, "1f"
